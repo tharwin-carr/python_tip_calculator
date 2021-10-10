492b551fdde984ab7841b24abb7016fd6fbaa55c
Tharwin Carr <tharwin.carr@tharwin.carr>
2021-10-10 12:45:33 -0700

    TC: first commit, finished project

---
+++ main.py
@@ -0,0 +1,17 @@
+bill = float(input("What was the total bill? $"))
+
+tip = int(input("How much would you like to tip? 10, 12, 15? "))
+
+people = int(input("How many people to split the bill?"))
+
+tip_as_percent = tip / 100
+
+total_tip_amount = bill * tip_as_percent
+
+total_bill = bill + total_tip_amount
+
+bill_per_person = total_bill / people
+
+final_amount = "{:.2f}".format(bill_per_person)
+
+print(f"Each person should pay ${final_amount}")
